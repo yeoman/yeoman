bf0dcee879c24bd1f77f1a41d4e5aba0a0984b85
addyosmani <addyosmani@gmail.com>
2012-05-16 16:37:13 +0100

    Removing node debug stuff previously pulled in.

--- cli/node_modules/html-minifier/v8/test.js
+++
@@ -1,8 +0,0 @@
-load('../src/htmlparser.js');
-load('../src/htmlminifier.js');
-
-var input = read('test.html'),
-    t1 = new Date(),
-    output = minify(input);
-
-print('minified in: ' + (new Date() - t1) + 'ms');
