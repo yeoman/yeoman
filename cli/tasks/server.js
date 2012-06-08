c692cdc728ada39baa046f31c93468bf8688bc0f
Mickael Daniel <daniel.mickael@gmail.com>
2012-06-08 12:59:29 +0200

    server: config and alias for server + watch

--- cli/tasks/server.js
+++ cli/tasks/server.js
@@ -4,14 +4,12 @@ var path = require('path'),
 
 module.exports = function(grunt) {
 
-  grunt.registerTask('server', 'Launch a preview, LiveReload compatible server', function() {
+  // Note: yeoman-server alone will exit prematurly unless `this.async()` is
+  // called. The task is designed to work alongside the `watch` task.
+  grunt.registerTask('yeoman-server', 'Launch a preview, LiveReload compatible server', function() {
     // Get values from config, or use defaults.
     var port = grunt.config('server.port') || 35729,
       base = path.resolve(grunt.config('server.base') || '.');
-
-    // This task is asynchronous, even though we don't actually call it.
-    // This ensures Grunt won't exit with this "long-running" task.
-    var cb = this.async();
 
     var middleware = [
       // Serve static files.
@@ -40,5 +38,12 @@ module.exports = function(grunt) {
 
     connect.apply(null, middleware).listen(port);
   });
+
+  // Retain grunt's built-in server task.
+  grunt.renameTask('server', 'grunt-server')
+
+  // The server task always run with the watch task, this is done by
+  // aliasing the server task to the relevant set of task to run.
+  grunt.registerTask('server', 'yeoman-server watch');
 
 };
