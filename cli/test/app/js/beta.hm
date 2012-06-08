da9354cfc4bfce80a9dc9cecc7a4efd3d6554ccc
addyosmani <addyosmani@gmail.com>
2012-06-08 23:03:29 +0100

    Adding in Harmony modules, Require HM for r.js testing later.

---
+++ cli/test/app/js/beta.hm
@@ -0,0 +1,9 @@
+
+// Simple module and a text comment that mentions module
+export var name = 'beta';
+
+export function func() {
+    return 'beta function';
+}
+
+export var uniqueBeta = 'uniqueBeta';
--- cli/test/app/js/main.js
+++ cli/test/app/js/main.js
@@ -1,6 +1,39 @@
-// Author: Thomas Davis <thomasalwyndavis@gmail.com>
 // Filename: main.js
 
-require(['views/app'], function(AppView){
-  var app_view = new AppView;
+require({
+	hm: 'hm',
+    paths: {
+    	jquery: 'libs/jquery/jquery-min',
+    	underscore: 'libs/underscore/underscore',
+    	backbone: 'libs/backbone/backbone',
+    	text: 'libs/require/text'
+  	},
 });
+
+require(['views/app', 'hm!alpha', 'jquery'], function(AppView, alpha){
+  var app_view = new AppView;
+
+  // Harmony testing
+
+	console.log(alpha);
+
+	var html = "";
+
+	$('.gamma').append(alpha.gammaName);
+	$('.beta').append(alpha.betaName);
+	$('.beta2').append(alpha.betaFunc());
+	$('.epsilon').append(JSON.stringify(alpha.epsilon));
+	$('.alpha').append(JSON.stringify(alpha));
+
+	// Shared state testing
+	alpha.shared.increment();
+	alpha.shared.increment();
+	$('.sharedState').append(alpha.shared.current());
+
+	// Parameterization
+	var safeDOM = alpha.SafeDOM;
+	var instance = alpha.DOMMunger.make(safeDOM);
+	instance.munge();
+
+});
+
