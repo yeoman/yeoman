6c4122f7f1c98f92ef9ee8ab0618b32c5f241200
addyosmani <addyosmani@gmail.com>
2012-06-19 18:37:23 +0100

    Pulling in revised r.js task to update requires config with runtime paths from @mklabs. Tested as working.

--- cli/tasks/rjs.js
+++ cli/tasks/rjs.js
@@ -1,5 +1,7 @@
-var path = require('path'),
-  rjs = require('requirejs');
+var fs = require('fs'),
+  path = require('path'),
+  rjs = require('requirejs'),
+  vm = require('vm');
 
 module.exports = function(grunt) {
   grunt.task.registerTask('rjs', 'Optimizes javascript that actually is built with requirejs.', function () {
@@ -9,6 +11,10 @@ module.exports = function(grunt) {
 
   grunt.registerHelper('rjs:optimize:js', function(options, cb) {
     if(!cb) { cb = options; options = {}; }
+
+    // update options to reflect the same paths configuration than runtime's
+    options = grunt.helper('rjs:paths', options);
+
     grunt.log.subhead('Options:');
     grunt.helper('inspect', options);
 
@@ -30,6 +36,43 @@ module.exports = function(grunt) {
 
       cb();
     });
+  });
+
+  // Given a rjs options Hash object, iterates through each `modules`, read
+  // its content and evaluates each one through node's VM. It looks up for
+  // `require({})` or `require.config({})` calls and get back the paths Hash
+  // object to update the rjs build configuration.
+  grunt.registerHelper('rjs:paths', function(options) {
+    var base = path.join(options.baseUrl, options.appDir);
+
+    // the sandbox object, this is the environment used to parse AMD modules
+    // looking up for runtime configuration
+    var sandbox = {};
+
+    // basic require shim.
+    sandbox.require = function require(o) {
+      if(!o || !o.paths) return;
+      // if the first arg passed to require is an Hash object, and has a paths
+      // property, pass it through require.config method
+      sandbox.require.config(o);
+    };
+
+    // require.config support, each time a rjs config with paths props
+    // appears, merge the paths configuration (and other options that might
+    // appear here) into the original options object.
+    sandbox.require.config = function config(o) {
+      grunt.utils._.extend(options, o);
+    };
+
+    // define noop
+    sandbox.define = function() {};
+
+    options.modules.forEach(function(mod) {
+      var body = grunt.file.read(path.join(base, mod.name + '.js'));
+      vm.runInNewContext(body, sandbox);
+    });
+
+    return options;
   });
 
 };
