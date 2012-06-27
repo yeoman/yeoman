437decdca01ef31831ddc946118017ff9364a95d
Paul Irish <paul.irish@gmail.com>
2012-06-26 23:53:26 -0700

    live reload the compiled stylesheets. awesome fix to pesky bug by @mklabs. :)

--- cli/tasks/init/yeoman/Gruntfile.js
+++ cli/tasks/init/yeoman/Gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function(grunt) {
       },
       compass: {
         files: ['css/sass/**/*.sass', 'css/sass/**/*.scss'],
-        tasks: 'shell:compass'
+        tasks: 'shell:compass reload'
       },
       // only used with `yeoman server`
       reload: {
