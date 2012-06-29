76c39c19871e871af564efd816cd9b299803988d
Mickael Daniel <daniel.mickael@gmail.com>
2012-06-29 04:32:23 +0200

    Add argument API for generators, creates an attribute getter for it.

--- cli/lib/generators/yeoman/controller/index.js
+++ cli/lib/generators/yeoman/controller/index.js
@@ -6,11 +6,25 @@ module.exports = Generator;
 
 function Generator() {
   yeoman.generators.Base.apply(this, arguments);
+
+  this.argument('name', {
+    type: String,
+    desc: 'The name of the controller to create'
+  });
+
+  this.argument('views', {
+    type: Array,
+    desc: 'The list of view name for this controller to create'
+  });
+
+  console.log('Name?', this.name);
+  console.log('Views?', this.views);
+
 }
 
 util.inherits(Generator, yeoman.generators.Base);
 
 Generator.prototype.createInitializerFile = function() {
   this.log.ok('Logging through grunt log API');
-  this.write('config/initializers/initializer.js', "# Add initialization content here");
+  this.write('app/controllers/' + this.name + '.js', "# Add initialization content here");
 };
