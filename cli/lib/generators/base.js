5e7c9604272388b1f1c4d758ef58d84c025d4fed
Mickael Daniel <daniel.mickael@gmail.com>
2012-06-29 04:32:23 +0200

    Add argument API for generators, creates an attribute getter for it.

--- cli/lib/generators/base.js
+++ cli/lib/generators/base.js
@@ -10,6 +10,8 @@ function Base(args, options) {
   events.EventEmitter.call(this);
   this.args = args;
   this.options = options;
+
+  this.arguments = [];
 }
 
 util.inherits(Base, events.EventEmitter);
@@ -37,6 +39,50 @@ Base.prototype.invoke = function invoke(name, config) {
   methods.forEach(function(method) {
     console.log('..', method, '..');
     self[method].apply(self, args);
+  });
+};
+
+//
+// Adds an argument to the class and creates an attribute getter for it.
+//
+// Arguments are different from options in several aspects. The first one
+// is how they are parsed from the command line, arguments are retrieved
+// from position:
+//
+//   yeoman generate NAME
+//
+// Instead of:
+//
+//   yeoman generate --name=NAME
+//
+// Besides, arguments are used inside your code as an accessor (self.argument),
+// while options are all kept in a hash (self.options).
+//
+
+Base.prototype.argument = function argument(name, config) {
+  console.log('create arg', name, config);
+  var self = this;
+
+  this.arguments.push({
+    name: name,
+    config: config
+  });
+
+  // create the accesor for this named argument, its value is direcly tied to
+  // the position of the given argument, and the order of specified arguments
+  // for this class.
+  this.__defineGetter__(name, function() {
+    var position = -1;
+    this.arguments.forEach(function(arg, i) {
+      if(position !== -1) return;
+      if(arg.name === name) position = i;
+    });
+
+    // a bit of coercion and type handling, to be improved (just dealing with
+    // Array / String, default is assumed to be String.)
+    //
+    // XXX add handling for Number, Array, String, Object (aka hash object)
+    return config.type === Array ? this.args.slice(position) : this.args[position];
   });
 };
 
