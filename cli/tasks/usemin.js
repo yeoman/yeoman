a7e54d360255508a25df8cd578cc074ec2bc53bd
addyosmani <addyosmani@gmail.com>
2012-07-09 11:49:12 +0100

    Adding more reference comments.

--- cli/tasks/usemin.js
+++ cli/tasks/usemin.js
@@ -6,25 +6,60 @@ var fs = require('fs'),
 // ### Usemin
 
 // Replaces references to non-optimized scripts or stylesheets
-// into a set of HTML files (or any templates/views) for the purposes
-// of wiring. 
-
+// into a set of HTML files (or any templates/views).
+//
 // The users markup should be considered the primary source of information 
 // for paths, references to assets which should be optimized.We also check 
 // against files present in the relevant directory () (e.g checking against 
 // the revved filename into the 'intermediate/') directory to find the SHA 
 // that was generated.
 //
-// Todo: Use a file dictionary during build process and rev task to
+// Todos:
+// * Use a file dictionary during build process and rev task to
 // store each optimized assets and their associated sha1.
+// * Update usemin-handler to follow @necolas proposed structure
 //
 // Thx to @krzychukula for the new, super handy replace helper.
 //
-// ### Usemin-handler
+// #### Usemin-handler
 // 
-// A special task which uses the build block HTML comments
-// in markup to get back the list of files to handle, and initialize the grunt
-// configuration appropriately, and automatically.
+// A special task which uses the build block HTML comments in markup to 
+// get back the list of files to handle, and initialize the grunt configuration 
+// appropriately, and automatically.
+//
+// Custom HTML "block" comments are provided as an API for interacting with the 
+// build script. These comments adhere to the following pattern:
+//
+//     <!-- build:<type> <path> -->
+//       ... HTML Markup, list of script / link tags.
+//     <!-- endbuild -->
+//
+// - type: is either js or css.
+// - path: is the file path of the optimized file, the target output. 
+//
+// An example of this in completed form can be seen below:
+//
+//    <!-- build:js js/app.js -->
+//      <script src="js/app.js"></script>
+//      <script src="js/controllers/thing-controller.js"></script>
+//      <script src="js/models/thing-model.js"></script>
+//      <script src="js/views/thing-view.js"></script>
+//    <!-- endbuild -->
+//
+// 
+// Internally, the task parses your HTML markup to find each of these blocks, and
+// initializes for you the corresponding Grunt config for the concat / min tasks
+// when `type=js`, the concat / css tasks when `type=css`.
+//
+// The task also handles use of RequireJS, for the scenario where you specify
+// the main entry point for your application using the "data-main" attribute
+// as follows:
+//
+//     <!-- build:js js/app.min.js -->
+//     <script data-main="js/main" src="js/vendor/require.js"></script>
+//     <!-- -->
+//
+// One doesn't need to specify a concat/min/css or rjs configuration anymore.
 //
 // Inspired by previous work in https://gist.github.com/3024891
 // For related sample, see: cli/test/tasks/usemin-handler/index.html
