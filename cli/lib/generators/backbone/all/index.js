3166bc932c648560cd5752c686ca69e39918c36b
addyosmani <addyosmani@gmail.com>
2012-07-18 14:06:56 +0100

    Add local wiring for adding scripts to app index.

--- cli/lib/generators/backbone/all/index.js
+++ cli/lib/generators/backbone/all/index.js
@@ -33,8 +33,31 @@ function Generator() {
 util.inherits(Generator, yeoman.generators.Base);
 
 Generator.prototype.injectBackbone = function injectBackbone() {
-  //this.template('lib/backbone-min.js', 'app/js/backbone.js');
-  //this.template('lib/underscore-min.js', 'app/js/underscore.js');
+
+    // Resolve path to index.html
+    var indexOut = path.resolve('index.html');
+
+    // Read in as string for further update
+    var indexData = this.readFileAsString(indexOut);
+    
+    // Workaround until copying underscore/lodash-like scripts don't cause issues.
+    indexData = this.appendScripts(indexData, 
+        'app/js/vendor.js', 
+        ['http://cdnjs.cloudflare.com/ajax/libs/lodash.js/0.4.1/lodash.min.js', 
+        'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min.js']);
+
+    // Wire MVC components (usemin: app/js/myapp.js)
+    indexData = this.appendScripts(indexData, 
+        'app/js/myapp.js', 
+        ['app/js/' + this.appname  +'.js',
+        'app/js/views/application-view.js',
+        'app/js/models/application-model.js',
+        'app/js/collections/application-collection.js',
+        'app/js/routes/app-router.js']);
+
+    // Write out final file
+    this.writeFileFromString(indexData, indexOut);
+
 };
 
 Generator.prototype.createDirLayout = function createDirLayout() {
