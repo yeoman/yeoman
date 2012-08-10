8530cb8316edf164a697739072b9afe23bf72cfd
Eric Bidelman <ebidel@gmail.com>
2012-08-10 14:13:42 -0700

    Fixes #192

--- cli/lib/plugins/insight.js
+++ cli/lib/plugins/insight.js
@@ -4,29 +4,31 @@ var fs = require('fs'),
     colors = require('colors'),
     prompt = require('prompt');
 
-
 module.exports = {
-    init : function(opts){
+  init: function(opts) {
 
-        // Store global state used throughout the project in the user's home dir.
-        // e.g. /Users/username/.yeoman
-        var YEOMAN_DIR = join(opts.getUserHome(), '.' + opts.pkgname);
+    // Store global state used throughout the project in the user's home dir.
+    // e.g. /Users/username/.yeoman
+    var YEOMAN_DIR = join(opts.getUserHome(), '.' + opts.pkgname);
 
-        var insightFolder = join(YEOMAN_DIR, 'insight');
+    // This should correspond to whatever name is used in package.json.
+    var INSIGHT_SCRIPT = '_' + opts.pkgname + 'insight'
 
-        // Assume yeomaninsight.py is installed in the user's home dir.
-        var insightRecordCmd = [join(insightFolder, 'yeomaninsight.py'), 'record'];
+    var insightFolder = join(YEOMAN_DIR, 'insight');
 
-        // Record this action in Insight (e.g. python yeomaninsight.py record cmd cmd2).
-        spawn('python', insightRecordCmd.concat(opts.cmds));
-        //insight.stdout.pipe(process.stdout);
+    // Assume yeomaninsight.py is installed globally (/usr/local/bin/)
+    var insightRecordCmd = ['record'];
 
-        fs.stat(join(insightFolder, '.log'), function(err, stats) {
-          // Error means file doesn't exist and this is the first run.
-          // Go through stat opt-in flow.
-          if ( !err )  {
-            return opts.cb();
-          }
+    // Record this action in Insight (e.g. python yeomaninsight.py record cmd cmd2).
+    spawn(INSIGHT_SCRIPT, insightRecordCmd.concat(opts.cmds));
+    //insight.stdout.pipe(process.stdout);
+
+    fs.stat(join(insightFolder, '.log'), function(err, stats) {
+      // Error means file doesn't exist and this is the first run.
+      // Go through stat opt-in flow.
+      if ( !err )  {
+        return opts.cb();
+      }
 
 /*jshint multistr:true */
 var msg = "\
@@ -36,31 +38,31 @@ May we anonymously report usage statistics to improve the tool over time? \n\
 More info: http://goo.gl/GPtU9 & http://yeoman.io".yellow + "\n\
 ==========================================================================".grey;
 
-          prompt.message = '[' + '?'.green + ']';
-          prompt.delimiter = ' ';
+      prompt.message = '[' + '?'.green + ']';
+      prompt.delimiter = ' ';
 
-          var properties = [{
-            name: 'optin',
-            message: '[Y/n]: ',
-            "default": 'Y',
-            validator: /^[yntf]{1}/i,
-            empty: false
-          }];
+      var properties = [{
+        name: 'optin',
+        message: '[Y/n]: ',
+        "default": 'Y',
+        validator: /^[yntf]{1}/i,
+        empty: false
+      }];
 
-          prompt.start();
-          console.log(msg);
-          prompt.get(properties, function(err, result) {
-            if (err) {
-              return opts.cb(err);
-            }
+      prompt.start();
+      console.log(msg);
+      prompt.get(properties, function(err, result) {
+        if (err) {
+          return opts.cb(err);
+        }
 
-            if (/n/i.test(result.optin)) {
-              spawn('python', insightRecordCmd.concat(['NO_STATS']));
-            }
-            opts.cb();
-          });
+        if (/n/i.test(result.optin)) {
+          spawn(INSIGHT_SCRIPT, insightRecordCmd.concat(['NO_STATS']));
+        }
+        opts.cb();
+      });
 
 
-        });
-    }
+    });
+  }
 };
