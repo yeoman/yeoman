35a6af33c9ef041b944a5ffecb652ff8d886e878
Addy Osmani <addyosmani@gmail.com>
2012-08-20 15:02:05 +0100

    Fixes #226, introducing a workaround flag for copying directories without processing through grunt. Starts on #144 with a basic backbone boilerplate generator.

---
+++ cli/lib/generators/bbb/app/templates/app/app.js
@@ -0,0 +1,83 @@
+define([
+  // Libraries.
+  "jquery",
+  "lodash",
+  "backbone",
+
+  // Plugins.
+  "plugins/backbone.layoutmanager"
+],
+
+function($, _, Backbone) {
+
+  // Provide a global location to place configuration settings and module
+  // creation.
+  var app = {
+    // The root path to run the application.
+    root: "/"
+  };
+
+  // Localize or create a new JavaScript Template object.
+  var JST = window.JST = window.JST || {};
+
+  // Configure LayoutManager with Backbone Boilerplate defaults.
+  Backbone.LayoutManager.configure({
+    paths: {
+      layout: "app/templates/layouts/",
+      template: "app/templates/"
+    },
+
+    fetch: function(path) {
+      path = path + ".html";
+
+      if (!JST[path]) {
+        $.ajax({ url: app.root + path, async: false }).then(function(contents) {
+          JST[path] = _.template(contents);
+        });
+      } 
+      
+      return JST[path];
+    }
+  });
+
+  // Mix Backbone.Events, modules, and layout management into the app object.
+  return _.extend(app, {
+    // Create a custom object with a nested Views object.
+    module: function(additionalProps) {
+      return _.extend({ Views: {} }, additionalProps);
+    },
+
+    // Helper for using layouts.
+    useLayout: function(name) {
+      // If already using this Layout, then don't re-inject into the DOM.
+      if (this.layout && this.layout.options.template === name) {
+        return this.layout;
+      }
+
+      // If a layout already exists, remove it from the DOM.
+      if (this.layout) {
+        this.layout.remove();
+      }
+
+      // Create a new Layout.
+      var layout = new Backbone.Layout({
+        template: name,
+        className: "layout " + name,
+        id: "layout"
+      });
+
+      // Insert into the DOM.
+      $("#main").empty().append(layout.el);
+
+      // Render the layout.
+      layout.render();
+
+      // Cache the refererence.
+      this.layout = layout;
+
+      // Return the reference, for chainability.
+      return layout;
+    }
+  }, Backbone.Events);
+
+});
---
+++ cli/lib/generators/bbb/app/templates/app/config.js
@@ -0,0 +1,29 @@
+// Set the require.js configuration for your application.
+require.config({
+
+  // Initialize the application with the main application file.
+  deps: ["main"],
+
+  paths: {
+    // JavaScript folders.
+    libs: "../assets/js/libs",
+    plugins: "../assets/js/plugins",
+
+    // Libraries.
+    jquery: "../assets/js/libs/jquery",
+    lodash: "../assets/js/libs/lodash",
+    backbone: "../assets/js/libs/backbone"
+  },
+
+  shim: {
+    // Backbone library depends on lodash and jQuery.
+    backbone: {
+      deps: ["lodash", "jquery"],
+      exports: "Backbone"
+    },
+
+    // Backbone.LayoutManager depends on Backbone.
+    "plugins/backbone.layoutmanager": ["backbone"]
+  }
+
+});
