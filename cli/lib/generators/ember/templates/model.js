65edbf9b919901a811ec1d993a7bdc4f7b23d2f6
addyosmani <addyosmani@gmail.com>
2012-08-23 22:49:15 +0100

    Adding re-written Ember-Rails generator for #246

--- cli/lib/generators/ember/templates/model.js
+++ cli/lib/generators/ember/templates/model.js
@@ -1,3 +1,5 @@
-<%= _.camelize(appname) %>.<%= _.classify(name) %> = DS.Model.extend({<% _.each(attrs, function(attr, i) { %>
-  <%= _.camelize(attr.name) %>: DS.attr('<%= attr.type %>')<% if(i < (attributes.length - 1)) { %>,<% } %>
-<% }); %>});
+
+// Requires Ember-Data
+// <%= _.camelize(appname) %>.<%= _.classify(name) %> = DS.Model.extend({<% _.each(attrs, function(attr, i) { %>
+//   <%= _.camelize(attr.name) %>: DS.attr('<%= attr.type %>')<% if(i < (attributes.length - 1)) { %>,<% } %>
+// <% }); %>});
