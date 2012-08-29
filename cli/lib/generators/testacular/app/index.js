5288012e4e2e4aa2371506048f787015b09fd0f3
Brian Ford <btford@umich.edu>
2012-08-29 14:02:09 -0400

    added testacular generator

---
+++ cli/lib/generators/testacular/app/index.js
@@ -0,0 +1,44 @@
+
+var util = require('util'),
+    path = require('path'),
+    yeoman = require('../../../../');
+
+module.exports = Generator;
+
+function Generator() {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.argument('interactive', {
+    type: Boolean,
+    defaults: true,
+    banner: '[interactive mode]',
+    required: false
+  });
+}
+
+util.inherits(Generator, yeoman.generators.Base);
+
+Generator.prototype.setupEnv = function setupEnv() {
+  // Copies the contents of the generator `templates`
+  // directory into your users new application path
+
+  this.directory('.','.', true);
+
+  // TODO: not sure if this is the most pragmatic way to do this
+  if (this.interactive) {
+    this.log.writeln("You may want to add the following to your Gruntfile.js:\n");
+    [
+      "    grunt.registerTask('test', 'run testacular', function () {",
+      "      var done = this.async();",
+      "      require('child_process').exec('testacular testacular.conf.js --single-run', function (err, stdout) {",
+      "        grunt.log.write(stdout);",
+      "        done();",
+      "      });",
+      "    });",
+      ""
+    ].forEach(function (ln) {
+      console.log(ln);
+    });
+  }
+
+};
---
+++ cli/lib/generators/testacular/app/templates/testacular.conf.js
@@ -0,0 +1,61 @@
+// Testacular configuration
+// Generated on Sun Aug 26 2012 01:57:11 GMT-0400 (EDT)
+
+
+// base path, that will be used to resolve files and exclude
+basePath = '';
+
+
+// list of files / patterns to load in the browser
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'test/spec/**/*.js'
+];
+
+
+// list of files to exclude
+exclude = [
+  
+];
+
+
+// test results reporter to use
+// possible values: dots || progress
+reporter = 'progress';
+
+
+// web server port
+port = 8080;
+
+
+// cli runner port
+runnerPort = 9100;
+
+
+// enable / disable colors in the output (reporters and logs)
+colors = true;
+
+
+// level of logging
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+logLevel = LOG_INFO;
+
+
+// enable / disable watching file and executing tests whenever any file changes
+autoWatch = false;
+
+
+// Start these browsers, currently available:
+// - Chrome
+// - ChromeCanary
+// - Firefox
+// - Opera
+// - Safari
+// - PhantomJS
+browsers = ['PhantomJS'];
+
+
+// Continuous Integration mode
+// if true, it capture browsers, run tests and exit
+singleRun = false;
