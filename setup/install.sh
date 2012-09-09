afe04e182c061c8d883aa69b52adfd704f097cbc
Tom Lane <theywerealltakensoichosethis@outlook.com>
2012-09-09 18:39:57 +0100

    Added link to find CLI tools if they are not detected.

--- setup/install.sh
+++ setup/install.sh
@@ -36,13 +36,13 @@ if [ "$MAC" -eq 1 ]; then
   if haveProg clang; then
     echo "CLI tools check 1/2 passed."
   else
-    echo "CLI tools not found."
+    echo "CLI tools not found. You can download them here: https://developer.apple.com/downloads"
     exit 1
   fi
   if haveProg lldb; then
     echo "CLI tools check 2/2 passed."
   else
-    echo "CLI tools not found."
+    echo "CLI tools not found. You can download them here: https://developer.apple.com/downloads"
     exit 1
   fi
 fi
